07b80dad7a218f4bd3ef7db2d0bf50d35d9d5af3
Evelyn Bart <evelyn.m.bart@gmail.com>
2023-08-21 12:38:31 -0400

    create mm & quiz components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [gamestate, useGameState] = useState("menu");
+
   return (
     <div className="App">
       <h1>Quiz App</h1>
---
+++ src/Components/MainMenu.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function MainMenu() {
+  return <div></div>;
+}
