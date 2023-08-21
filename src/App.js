85e94a2b48129d2ad1842020ce444cd4c5a36702
Evelyn Bart <evelyn.m.bart@gmail.com>
2023-08-21 12:31:19 -0400

    add title and css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+    </div>
+  );
 }
 
 export default App;
